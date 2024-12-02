04138d1e437e71b142c80a42578a47f9c69583c9
saens <songsh5775@gmail.com>
2024-12-02 23:17:51 +0900

    과제6-1: LCS2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class Main {
 
-    static int get(int x, int y, int[][] dp) {
+    static int[][] dp;
+
+    static int get(int x, int y) {
         return x < 0 || y < 0 ? 0 : dp[y][x];
     }
 
@@ -12,12 +14,22 @@ public class Main {
         String sy = scanner.nextLine(), sx = scanner.nextLine();
         int leny = sy.length(), lenx = sx.length();
 
-        int[][] dp = new int[leny][lenx];
+        dp = new int[leny][lenx];
         for(int y=0 ; y<leny ; ++y) for(int x=0 ; x<lenx ; ++x) {
-            if(sy.charAt(y) == sx.charAt(x)) dp[y][x] = get(x-1, y-1, dp) + 1;
-            else dp[y][x] = Math.max(get(x-1, y, dp), get(x, y-1, dp));
+            if(sy.charAt(y) == sx.charAt(x)) dp[y][x] = get(x-1, y-1) + 1;
+            else dp[y][x] = Math.max(get(x-1, y), get(x, y-1));
+        }
+
+        int x = lenx-1, y = leny-1;
+        StringBuilder sb = new StringBuilder();
+
+        while(x >= 0 && y >= 0) {
+            if (sx.charAt(x) == sy.charAt(y)) { sb.append(sy.charAt(y--)); x--; }
+            else if(get(x-1, y) > get(x, y-1)) x--;
+            else y--;
         }
 
         System.out.println(dp[leny-1][lenx-1]);
+        System.out.println(sb.reverse());
     }
 }
